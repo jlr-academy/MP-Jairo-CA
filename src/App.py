a7ac82b0374379cf65bb82dcdf1914b3405740ec
Piotr-Koper <pkoper@jaguarlandrover.com>
2021-10-04 15:15:36 +0100

    extra comment added in line 6

--- src/App.py
+++ src/App.py
@@ -3,6 +3,8 @@ from File_handlers.txt import read, write
 from Tools.uitilities import create_menu, print_list, no_orders
 import os
 
+#PK comments
+
 #Read txt data if any
 list_product = read('C:/Users/jcanoalo/Desktop/IW/Mini_project_1/Data', "Product_list.txt")
 list__courier = read('C:/Users/jcanoalo/Desktop/IW/Mini_project_1/Data', "Courier_list.txt")
