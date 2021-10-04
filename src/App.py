8cb723e14674784b4efad083c3c539e3c13ba824
Piotr-Koper <pkoper@jaguarlandrover.com>
2021-10-04 15:20:24 +0100

    extra comment added in line 6

--- src/App.py
+++ src/App.py
@@ -3,7 +3,7 @@ from File_handlers.txt import read, write
 from Tools.uitilities import create_menu, print_list, no_orders
 import os
 
-#PK comments
+#PK commentsgit status
 
 #Read txt data if any
 list_product = read('C:/Users/jcanoalo/Desktop/IW/Mini_project_1/Data', "Product_list.txt")
