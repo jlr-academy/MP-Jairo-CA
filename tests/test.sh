5e21f4245eb0726d058a196a77d7e8fc2d900e95
jcanoalo <jcanoalo@jaguarlandrover.com>
2021-10-25 15:34:24 +0100

    shell scripting

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $HOME
 echo -n "Enter file name or pytest to run all: "
 read var
 
@@ -10,7 +9,7 @@ if [[ $exit_code = 0 ]]; then
     echo "success"
     git add .
     git commit -m "shell scripting"
-    git push
+    git push origin master
 else
     echo "failure: $exit_code"
 fi
